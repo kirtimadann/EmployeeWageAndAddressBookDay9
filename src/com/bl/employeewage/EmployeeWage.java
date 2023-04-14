ada6490ed7630c05b3530c7cf23192d8d9486708
lunainfotec <kirtimadan1997@gmail.com>
2023-04-14 23:20:44 +0530

    added : Employee wage for multiple company

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -4,20 +4,18 @@ public class EmployeeWage {
 
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int EMP_WORKING_DAY = 20;
-    public static final int EMP_WORKING_HRS= 100;
 
 
-    public void empWageMethod(){
+
+    public int ComputeempWage(String company,int emp_rate_per_hours, int emp_working_day ,int max_emp_working_hrs){
 
         int empHrs = 0;
         int totalEmpHrs = 0;
-        int totalEmpDays = 0;
+        int totalworkingEmpDays = 0;
 
-        while (totalEmpHrs <= EMP_WORKING_HRS && totalEmpDays < EMP_WORKING_DAY){
+        while (totalEmpHrs <= max_emp_working_hrs && totalworkingEmpDays < emp_working_day){
 
-            totalEmpDays++;
+            totalworkingEmpDays++;
 
             int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
@@ -39,21 +37,23 @@ public class EmployeeWage {
 
             }
             totalEmpHrs = empHrs + totalEmpHrs;
-            System.out.println("day "+totalEmpDays+ ": " +totalEmpHrs + " Hours");
+            System.out.println("day "+totalworkingEmpDays+ ": " +totalEmpHrs + " Hours");
 
         }
 
-        int empwage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println(empwage);
+        int empwage = totalEmpHrs * emp_rate_per_hours;
+        System.out.println("totalempwage :"+ empwage);
+        return empwage;
 
     }
 
     public static void main(String[] args) {
 
         EmployeeWage obj = new EmployeeWage();
-        obj.empWageMethod();
 
+        System.out.println(obj.ComputeempWage("wipro",20,20,100));
 
+        System.out.println(obj.ComputeempWage("softtech",10,10,50));
 
     }
 }
